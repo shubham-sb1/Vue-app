8378903fdbcaa9ef92f3f0e906509155789f1ee3
Shubham <shubhambajoria01@gmail.com>
2025-03-25 15:25:00 +0530

    NAV BAR CHANGE TEST

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -19,7 +19,7 @@ return route.path === routePath;
             <RouterLink class="flex flex-shrink-0 items-center mr-4" to="/">
               <img class="h-10 w-auto" :src="logo" alt="Vue Jobs" />
               <span class="hidden md:block text-white text-2xl font-bold ml-2"
-                >Vue Jobs</span
+                >Vue Jobs test</span
               >
             </RouterLink>
             <div class="md:ml-auto">
